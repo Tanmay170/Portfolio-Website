bd3803174b8f549b130ee8a6667fa861da07aff2
Tanmay Bansal <tanmaybansal170@gmail.com>
2024-10-05 10:04:47 +0530

    Projects update

--- Portfolio/my-project/src/Data/Data.js
+++ Portfolio/my-project/src/Data/Data.js
@@ -1,7 +1,10 @@
 import Currency from '../Assets/Currency.jpg'
 import React from '../Assets/data.jpg'
 import CN from '../Assets/CN.jpg'
-import Travel from '../Assets/Travel.jpg'
+import Volt from '../Assets/Volt.png'
+import Quiz from '../Assets/Quiz.png'
+import Hangman from '../Assets/Hangman.png' 
+import Gen from '../Assets/Gen.png'
 
 export const data=[
     {
@@ -27,10 +30,31 @@ export const data=[
     },
     {
         id:4,
-        name:"Travel Website",
-        image:Travel,
-        github:"",
-        live:"",
+        name:"Volt Website",
+        image:Volt,
+        github:"https://github.com/Tanmay170/Volt-site",
+        live:"https://tanmay170.github.io/Volt-site/",
+    },
+    {
+        id:5,
+        name:"Hangman",
+        image:Hangman,
+        github:"https://github.com/Tanmay170/Hangman",
+        live:"https://hangman-weld-seven.vercel.app/",
+    },
+    {
+        id:6,
+        name:"Quiz",
+        image:Quiz,
+        github:"https://github.com/Tanmay170/Harry-Potter-Quiz",
+        live:"https://harry-potter-quiz-dun.vercel.app/",
+    },
+    {
+        id:7,
+        name:"Certificate Generator",
+        image:Gen,
+        github:"https://github.com/Tanmay170/Certificate-Generator",
+        live:"https://certificate-generator-mu.vercel.app/",
     },
 
 
